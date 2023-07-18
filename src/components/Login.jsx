5bb27f86f54875d3de0000365261496fe7d78fce
kadirtetik1 <tetik.kadir@hotmail.com>
2023-07-18 12:23:42 +0300

    first

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from './Login.module.css'
 import {FiX} from "react-icons/fi";
+import {GiTeacher} from "react-icons/gi";
+import {PiStudentFill} from "react-icons/pi";
 
 const Login = (props) => {
     return props.isClicked ? (
@@ -12,10 +14,61 @@ const Login = (props) => {
             <div className={styles.close}  onClick={() => props.closeLogin(false)}><FiX size={'2rem'}/></div>
             </div>
 
-                <h1>Login</h1>
-                <h2>Giriş Yap:</h2>
-                <h2>Giriş Yap:</h2>
-                <h2>Giriş Yap:</h2>
+               <form className={styles.formContainer}>
+
+
+                <div className={styles.roleBox}>
+
+                  <div className={styles.selectRole}>
+
+                  <input type="radio" id="teacher" name="akademik_unvan" value="teacher" required="required"/>
+                  <div className={styles.inputTitle}>
+                    <GiTeacher className={styles.icon} size={'2rem'} color={"#feb206"}/>
+                 <label style={{fontSize:"11px", fontWeight:"600"}} for="teacher">Öğretmen</label>
+                 </div>
+
+                  </div>
+
+                  <div className={styles.selectRole}>
+
+                  <input type="radio" id="student" name="akademik_unvan" value="student" required="required"/>
+                  <div className={styles.inputTitle}>
+                  <PiStudentFill className={styles.icon} size={'2rem'} color={"#feb206"}/>
+                 <label style={{fontSize:"11px", fontWeight:"600"}} for="student">Öğrenci</label>
+                  </div>
+                 
+                </div>
+
+                  <h4 style={{padding:"0", margin:"0"}}>Akademik Ünvanınızı Seçiniz</h4>
+                
+
+                  </div>
+
+
+                <div className={styles.inputBox}>
+                    <input type="text" required="required"/>
+                    <span>Kullanıcı Adı</span>
+                    <i></i>
+                </div>
+
+                <div className={styles.inputBox}>
+                    <input type="password" required="required"/>
+                    <span>Şifre</span>
+                    <i></i>
+                </div>
+
+                <div className={styles.links}>
+
+                    <a href='#'>Şifremi Değiştir</a>
+                    <a href='#'>Şifremi Unuttum</a>
+
+                </div>
+
+                <div className={styles.buttonContainer}>
+                <input className={styles.girisButton} type="submit" value="Giriş Yap"/>
+                </div>
+                
+               </form>
             </div>
           
         </div>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import styles from './Register.module.css'
 import {FiX} from "react-icons/fi";
+import {GiTeacher} from "react-icons/gi";
+import {PiStudentFill} from "react-icons/pi";
+
 
 const Register = (props) => {
   return props.isClicked ? (
@@ -12,10 +15,71 @@ const Register = (props) => {
             <div className={styles.close}  onClick={() => props.closeRegister(false)}><FiX size={'2rem'}/></div>
         </div>
 
-            <h1>Register</h1>
-            <h2>Giriş Yap:</h2>
-            <h2>Giriş Yap:</h2>
-            <h2>Giriş Yap:</h2>
+        <form className={styles.formContainer}>
+
+          <div className={styles.roleBox}>
+
+                  <div className={styles.selectRole}>
+
+                  <input type="radio" id="teacher" name="akademik_unvan" value="teacher" required="required"/>
+                  <div className={styles.inputTitle}>
+                    <GiTeacher className={styles.icon} size={'2rem'} color={"#0076fa"}/>
+                 <label style={{fontSize:"11px", fontWeight:"600"}} for="teacher">Öğretmen</label>
+                 </div>
+
+                  </div>
+
+                  <div className={styles.selectRole}>
+
+                  <input type="radio" id="student" name="akademik_unvan" value="student" required="required"/>
+                  <div className={styles.inputTitle}>
+                  <PiStudentFill className={styles.icon} size={'2rem'} color={"#0076fa"}/>
+                 <label style={{fontSize:"11px", fontWeight:"600"}} for="student">Öğrenci</label>
+                  </div>
+                 
+                </div>
+
+                  <h4 style={{padding:"0", margin:"0"}}>Akademik Ünvanınızı Seçiniz</h4>
+                
+
+                  </div>
+                
+        <div className={styles.inputBox}>
+                    <input type="text" required="required"/>
+                    <span>İsim</span>
+                    <i></i>
+                </div>
+
+                <div className={styles.inputBox}>
+                    <input type="text" required="required"/>
+                    <span>Soyisim</span>
+                    <i></i>
+                </div>
+                
+                <div className={styles.inputBox}>
+                    <input type="text" required="required"/>
+                    <span>E-mail</span>
+                    <i></i>
+                </div>
+
+
+                <div className={styles.inputBox}>
+                    <input type="text" required="required"/>
+                    <span>Kullanıcı Adı</span>
+                    <i></i>
+                </div>
+
+                <div className={styles.inputBox}>
+                    <input type="password" required="required"/>
+                    <span>Şifre</span>
+                    <i></i>
+                </div>
+
+                <div className={styles.buttonContainer}>
+                <input className={styles.uyeButton} type="submit" value="Üye Ol"/>
+                </div>
+                
+               </form>
         </div>
       
     </div>
