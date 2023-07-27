e6b6cb470eeff0d177da44555c8754e7e3bb9cf1
kadirtetik1 <tetik.kadir@hotmail.com>
2023-07-27 12:38:59 +0300

    proplar eklendi

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -2,26 +2,26 @@ import React from 'react'
 import styles from './CourseCard.module.css'
 import {motion} from "framer-motion";
 
-const CourseCard = () => {
+const CourseCard = (props) => {
   return (
     <div className={styles.cardContainer}>
 
 <div className={styles.teacherNameImage}>
-<div className={styles.teacherName}><h4>Prof.Dr. Ahmet Yılmaz</h4></div>
-<div className={styles.teacherImage}><img src ="https://t4.ftcdn.net/jpg/02/90/27/39/360_F_290273933_ukYZjDv8nqgpOBcBUo5CQyFcxAzYlZRW.jpg" style={{height:"100%", width:"100%", borderRadius:"50%"}} /></div>
+<div className={styles.teacherName}><h4> {props.unvan} {props.name} </h4></div>
+<div className={styles.teacherImage}><img src ={props.teacherImage} style={{height:"100%", width:"100%", borderRadius:"50%"}} /></div>
 </div>
 
-<div className={styles.courseImage}><img src ="https://web.harran.edu.tr/assets/uploads/sites/57/slides/ef38464dd6a24fa7bb675710d466fb31.jpg" style={{height:"100%", width:"100%", borderRadius:"10px"}} /></div>
+<div className={styles.courseImage}><img src ={props.courseImage} style={{height:"100%", width:"100%", borderRadius:"10px"}} /></div>
 
 
   <div className={styles.departmentInfos}>
-  <div className={styles.courseDepartment}>Makine Mühendisliği</div>
-  <div className={styles.period}>Güz</div>
+  <div className={styles.courseDepartment}>{props.departmentName}</div>
+  <div className={styles.period}>{props.period}</div>
   </div>
   
   <div className={styles.courseInfo}>
-  <div className={styles.courseTitle}>Statik & Mukavemet</div>
-  <div className={styles.kontenjan}> <div className={styles.takenKont}>40</div>/<div className={styles.openKont}>40</div> </div>
+  <div className={styles.courseTitle}>{props.courseTitle}</div>
+  <div className={styles.kontenjan}> <div className={styles.takenKont}>{props.takenKont}</div>/<div className={styles.openKont}>{props.openKont}</div> </div>
 
 </div>
 
--- src/screens/students/StudentsHome.jsx
+++ src/screens/students/StudentsHome.jsx
@@ -11,7 +11,10 @@ const StudentsHome = () => {
       
       <StuNavbar/>
       <StuHero/>
-      <CourseCard/>
+      <CourseCard unvan="Prof.Dr." name="Mehmet Yılmaz" teacherImage="https://t4.ftcdn.net/jpg/02/90/27/39/360_F_290273933_ukYZjDv8nqgpOBcBUo5CQyFcxAzYlZRW.jpg"
+      courseImage="https://web.harran.edu.tr/assets/uploads/sites/57/slides/ef38464dd6a24fa7bb675710d466fb31.jpg"
+      departmentName="Makine Mühendisliği" period="Güz" courseTitle="Statik & Mukavemet" takenKont="39" openKont="40"
+      />
       
       
     </div>
